dabe11a7097ce9d624dd0af271d1909276616349
diuguide <61917141+diuguide@users.noreply.github.com>
2020-09-23 13:17:20 -0400

    react boilerplate

---
+++ src/components/Landing.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Jumbotron from './components/Jumbotron'
+
+function Landing() {
+    return (
+        <Jumbotron />
+    )
+}
+
+export default Landing
