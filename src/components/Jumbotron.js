8112edfcd65248d42f713541b44e1652664bd0f6
diuguide <61917141+diuguide@users.noreply.github.com>
2020-09-26 18:52:45 -0400

    added sort by drop down

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 function Jumbotron(props) {
-    return(
+    return (
         <div className="jumbotron bg-primary">
             <h1>Employee Directory</h1>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Sort By
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                    <button class="dropdown-item" type="button">Gender</button>
+                    <button class="dropdown-item" type="button">DOB Ascending</button>
+                    <button class="dropdown-item" type="button">DOB Descending</button>
+                </div>
+            </div>
         </div>
     )
 }
