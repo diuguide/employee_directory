df7373a8263f45be35f2cffed22f1e274dc7422c
diuguide <61917141+diuguide@users.noreply.github.com>
2020-09-27 20:11:19 -0400

    filter and map working properly

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,48 +1,21 @@
 import React from 'react';
 import "../styles/Entry.css";
 function Entry(props) {
-    const filterGender = props.data.filter(person => person.gender.includes("male"));
-    console.log(filterGender);
-    console.log(props.data)
-    // function filter() {
-    // let filteredName = props.data.filter(person => person.gender.includes('male')).map(filteredName => (
-    //         <li>
-                
-    //             <h1>{filteredName} Hello World </h1>
-    //         </li>
-    //     ))
-
-    // console.log(filteredName)
-        
-    // }
-    // filter();
+    const filterGender = props.data.filter(person => person.gender === ("male"));
     return (
         <div className="row">
             <div className="col">
-                <div><h1> hellow world</h1>
-                    {filterGender.map(filteredName => (
-                        <li>
-                            
-                            <h1>{filteredName} Hello World </h1>
-                        </li>
-                    ))}
+                {filterGender.map((male) => 
+                <li key={male.cell}>
+                    <img alt="profile" src={male.picture.large}></img>
+                   <h3> {male.name.first} {male.name.last} {male.gender} </h3> 
+                </li>
+                )}
+                <div>
+                    
                 </div>
-                {/* <ul className="list-group">
-                {props.data.map(result => (
-                    <li className="list-group-item" key={result.id}>
-                        <img alt={result.gender} className="img-fluid" src={result.picture.large} ></img>
-                        <h3> {result.email} </h3>
-                        <h3> {result.dob.date} </h3>
-                        <h3> {result.gender} </h3>
-                        <h3> {result.phone} </h3>
-                    </li>
-                ))}
-            </ul> */}
             </div>
         </div>
     )
-
-
 }
-
 export default Entry;
--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -4,15 +4,15 @@ function Jumbotron(props) {
     return (
         <div className="jumbotron bg-primary">
             <h1>Employee Directory</h1>
-            <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Sort By
                 </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                    <button class="dropdown-item" type="button">Filter Men</button>
-                    <button class="dropdown-item" type="button">Filter Women</button>
-                    <button class="dropdown-item" type="button">DOB Ascending</button>
-                    <button class="dropdown-item" type="button">DOB Descending</button>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
+                    <button className="dropdown-item" type="button">Filter Men</button>
+                    <button className="dropdown-item" type="button">Filter Women</button>
+                    <button className="dropdown-item" type="button">DOB Ascending</button>
+                    <button className="dropdown-item" type="button">DOB Descending</button>
                 </div>
             </div>
         </div>
