b47c7315e49defc9bce8e411d01e6c3391092613
diuguide <61917141+diuguide@users.noreply.github.com>
2020-09-30 13:49:43 -0400

    added descending and ascending buttons

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import "../styles/Entry.css";
 function Entry(props) {
-    function refreshBtn() {
-        window.location.reload()
-    }
+    
     return (
         <div className="row">
             <div className="col">
-                <div className="row">
-                    <div className="col mb-4 ml-4">
-                        <div className="dropdown">
-                            <button className="btn btn-secondary dropdown-toggle shadow" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Sort By
-                            </button>
-                            <button className="btn btn-secondary ml-4 shadow" type="button" id="refreshBtn" onClick={() => refreshBtn()}>Refresh</button>
-                            <div className="dropdown-menu shadow" aria-labelledby="dropdownMenu2">
-                                <button className="dropdown-item" type="button" onClick={() => { props.filterMen() }}>Filter Men</button>
-                                <button className="dropdown-item" type="button" onClick={() => { props.filterWomen() }}>Filter Women</button>
-                                <button className="dropdown-item" type="button">DOB Ascending</button>
-                                <button className="dropdown-item" type="button">DOB Descending</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                
                 <div className="row">
                     <div className="col">
                         <table className="shadow">
@@ -40,6 +23,7 @@ function Entry(props) {
                                     <td>{entry.name.last}</td>
                                     <td>{entry.gender}</td>
                                     <td>#{entry.phone}</td>
+                                    <td>{entry.dob.age}</td>
                                 </tr>
                             )}
                         </table>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,43 +2,54 @@ import React from 'react';
 import axios from 'axios';
 import Entry from '../components/Entry'
 import Jumbotron from '../components/Jumbotron'
+import moment from 'moment';
 
 class Home extends React.Component {
     state = {
         data: [],
+        originalData: []
     }
     componentDidMount() {
         axios('https://randomuser.me/api/?results=12',
         )
             .then((res) => {
                 console.log("Success Component Did Mount: " + res.data.results) 
-                this.setState({ data: res.data.results })
+                this.setState({ data: res.data.results, originalData: res.data.results })
             }
             );
     }
-
     filterMen = () => {
-        const filteredMen = this.state.data.filter(person => person.gender === ("male"))
+        const filteredMen = this.state.originalData.filter(person => person.gender === ("male"))
         console.log(filteredMen)
         this.setState({ data: filteredMen}) 
     }
     filterWomen = () => {
-        const filteredWomen = this.state.data.filter(person => person.gender === ("female"))
+        const filteredWomen = this.state.originalData.filter(person => person.gender === ("female"))
         // console.log(filteredWomen)
         this.setState({ data: filteredWomen }) 
     }
-    // filterDobAsc = () => {
-    //     console.log(this.data.)
-    // }
+    filterDobAsc = () => {
+        const ageAsc = this.state.originalData.sort((a,b) => parseInt(a.dob.age)  - parseInt(b.dob.age))
+        this.setState({ data: ageAsc})
+    }
+    filterDobDsc = () => {
+        const ageDsc = this.state.originalData.sort((a,b) => parseInt(b.dob.age)  - parseInt(a.dob.age))
+        this.setState({ data: ageDsc})
+    }
+    refreshBtn = () => {
+        window.location.reload()
+    }
     render() {
 
         return (
             <div>
-                <Jumbotron />
-                <Entry 
-                 data={this.state.data}
+                <Jumbotron   
                  filterMen={this.filterMen}
                  filterWomen={this.filterWomen}
+                 filterDobAsc={this.filterDobAsc}
+                 filterDobDsc={this.filterDobDsc}
+                 refreshBtn={this.refreshBtn} />
+                <Entry data={this.state.data}
                   />
             </div>
         )
